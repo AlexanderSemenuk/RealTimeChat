1d8c5c01aaa8a867199fc90d201d3e2df9f840df
Alexander Semenuk <alexandersemenuk212@gmail.com>
2024-07-25 19:08:08 +0300

    work in progress

--- backend/Chat/Chat/Hubs/ChatHub.cs
+++ backend/Chat/Chat/Hubs/ChatHub.cs
@@ -1,5 +1,7 @@
-﻿using Chat.Models;
+﻿using System.Text.Json;
+using Chat.Models;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Caching.Distributed;
 
 namespace Chat.Hubs;
 
@@ -10,13 +12,55 @@ public interface IChatClient
 
 public class ChatHub : Hub<IChatClient>
 {
+    private readonly IDistributedCache _cache;
+    public ChatHub(IDistributedCache cache)
+    {
+        _cache = cache;
+    }
     public async Task JoinChat(UserConnection connection)
     {
         await Groups.AddToGroupAsync(Context.ConnectionId, connection.ChatRoom);
 
+        var stringConnection = JsonSerializer.Serialize(connection);
+        
+        await _cache.SetStringAsync(Context.ConnectionId, stringConnection);
+        
         await Clients
             .Group(connection.ChatRoom)
-            .ReceiveMessage("Admin", $"{connection.UserName} присоединился");
+            .ReceiveMessage("Admin", $"{connection.UserName} joined");
+    }
+
+    public async Task SendMessage(string message)
+    {
+        var stringConnection = await _cache.GetAsync(Context.ConnectionId);
+
+        var connection = JsonSerializer.Deserialize<UserConnection>(stringConnection);
+
+        if (connection is not null)
+        {
+            await Clients
+                .Group(connection.ChatRoom)
+                .ReceiveMessage(connection.UserName, message);
+        }
+
+    }
+
+    public override async Task OnDisconnectedAsync(Exception? exception)
+    {
+        var stringConnection = await _cache.GetAsync(Context.ConnectionId);
+        var connection = JsonSerializer.Deserialize<UserConnection>(stringConnection);
+
+        if (connection is not null)
+        {
+            await _cache.RemoveAsync(Context.ConnectionId);
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, connection.ChatRoom);
+
+            await Clients
+                .Group(connection.ChatRoom)
+                .ReceiveMessage("Admin", $"{connection.UserName} left the chat");
+        }
+        
+        await base.OnDisconnectedAsync(exception);
     }
 }
 
--- backend/Chat/Chat/Program.cs
+++ backend/Chat/Chat/Program.cs
@@ -2,6 +2,12 @@ using Chat.Hubs;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    var connection = builder.Configuration.GetConnectionString("Redis");
+    options.Configuration = connection;
+});
+
 builder.Services.AddCors(options =>
 {
     options.AddDefaultPolicy(policy =>
@@ -14,6 +20,9 @@ builder.Services.AddCors(options =>
 });
 
 builder.Services.AddSignalR();
+
+
+
 var app = builder.Build();
 
 app.UseCors();
